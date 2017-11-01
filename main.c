8233d7204d95ed389ced220afc238cb4306e4950
Piotr Milewski <pmilewski@stuy.edu>
2017-10-31 22:36:50 -0400

    added header and finished extra stuff

--- main.c
+++ main.c
@@ -3,47 +3,79 @@
   HW08
   2017-10-31*/
 
+#include "main.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <string.h>
+#include <errno.h>
 
-char *prettyBytes( long unsigned buff, int B, char *s){
+void print_error(int err){
+  if (err == -1){
+    printf("%s\n", strerror(errno));
+    exit(1);
+  }
+}
+
+char *prettyBytes( long unsigned buff, int B, char *retStr){
   if (buff / 1024 >= 1){
-    prettyBytes( buff / 1024, ++B, s);
+    prettyBytes( buff / 1024, ++B, retStr);
   }
   else{
-    sprintf( s, "%lu", buff);
+    sprintf( retStr, "%lu", buff);
     if (B == 0){
-      strcat( s, "B");
+      strcat( retStr, "B");
     }
     else if (B == 1){
-      strcat( s, "KB");
+      strcat( retStr, "KB");
     }
     else if (B == 2){
-      strcat( s, "MB");
+      strcat( retStr, "MB");
     }
     else if (B == 3){
-      strcat( s, "GB");
+      strcat( retStr, "GB");
     }
     else if (B == 4){
-      strcat( s, "TB");
+      strcat( retStr, "TB");
     }
   }
-  return s;
-}
-/*
-char *convertMode( int perm, char *s){
-  
-
+  return retStr;
 }
 
-char *lsify( int perm, char *s){
-
-
+//get current status of bit in perm
+char getBitMode( int permissions, int bit){
+  return (permissions >> bit) & 1; //shift to the right "bit" times and check if 1st digit is a 1
 }
-*/
+
+char *convertMode( int permissions, char *retStr){
+  char currMode; //sets current permission mode
+  retStr[0] = '-'; //only checking files, not directories or links
+  int i = 1;
+  for (; i <= 9; i++) {
+    if (i % 3 == 1)
+      currMode = 'r';
+    else if (i % 3 == 2)
+      currMode = 'w';
+    else if (i % 3 == 0)
+      currMode = 'e';
+    //check if current bit is a 1
+    if (getBitMode( permissions, 9-i))
+      retStr[i] = currMode;
+    else
+      retStr[i] = '-';
+  }
+  retStr[10] = 0; //null terminator
+  return retStr;
+}
+
+char *lsifyPerms( int permissions, char *retStr){
+  char modeStr[16];
+  strcat( retStr, convertMode( permissions, modeStr));
+}
+
 int main(){
 
   struct stat buff;
@@ -53,14 +85,11 @@ int main(){
   printf("Mode of main.c: %o\n", buff.st_mode);
   printf("Time of last access: %s\n", ctime(&buff.st_atime));
 
-  char s[256];
-  printf("\nCoherent file size of main.c: %s\n", prettyBytes(buff.st_size, 0, s));
+  char retStr[256];
+  printf("Coherent file size of main.c: %s\n", prettyBytes( buff.st_size, 0, retStr));
 
-  //printf("%s\n", lsify( buff.st_mode, 
-  //st_mode 
-  //st_uid id of owner
-  //st_gid id of group
-  //ctime
-  //file name
+  char retStr2[1024];
+  printf("\nPermissions in the ls -l format: %s\n", lsifyPerms( buff.st_mode, retStr2));
+
   return 0;
 }
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN
+#define MAIN
+
+void print_error( int );
+char *prettyBytes( long unsigned, int, char * );
+char getBitMode( int, int );
+char *convertMode( int, char * );
+char *lsifyPerms( int, char * );
+int main();
+
+#endif
